79c6590fbc2b5dd1cf410d8f56e68ae7f0e4e04a
rhlCoder08120 <55622074+rhlCoder08120@users.noreply.github.com>
2019-11-03 00:10:08 +0530

    delete

--- social-media/target/classes/data.sql
+++
@@ -1,9 +0,0 @@
-insert into User values('1',sysdate(),'rahul kumar pandey');
-insert into User values('2',sysdate(),'Brijesh kumar pandey');
-insert into User values('3',sysdate(),'Suraj kumar pandey');
-insert into User values('4',sysdate(),'Shivam kumar pandey');
-insert into User values('5',sysdate(),'Shashi kumar pandey');
-insert into User values('6',sysdate(),'Ravi kumar pandey');
-insert into User values('7',sysdate(),'Birendra pandey');
-insert into User values('9',sysdate(),'Rajesh pandey');
-insert into User values('10',sysdate(),'Indrasen pandey');
